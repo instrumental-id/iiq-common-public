1d1aa4fd241c985b3b7eda4047ae03afa0468ae5
Devin Rosenbauer <devin@identityworksllc.com>
2023-08-02 15:43:18 -0400

    Docs

--- src/com/identityworksllc/iiq/common/plugin/annotations/ResponsesAllowed.java
+++ src/com/identityworksllc/iiq/common/plugin/annotations/ResponsesAllowed.java
@@ -5,9 +5,18 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * The annotation specifying what VO objects can be validly returned from a
+ * REST API endpoint method, apart from the defaults.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.TYPE})
 public @interface ResponsesAllowed {
 
+    /**
+     * The list of classes that the REST API endpoint supports. All other output
+     * types will result in a logged warning and a null response.
+     * @return The list of classes
+     */
     Class<?>[] value();
 }
