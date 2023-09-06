5a6debd79aef274685ddcab96205d84ed1c114a8
Devin Rosenbauer <devin@identityworksllc.com>
2023-09-06 13:28:10 -0400

    Incorporate checkstyle into build maybe

---
+++ scripts/ci/checkstyle-to-gitlab.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# From here, modified a bit for MacOS tools
+# https://github.com/CodeDefenders/CodeDefenders/blob/master/scripts/ci/generate-codequality-report-from-checkstyle.sh
+
+function mapSeverity() {
+    severity="$1"
+
+    if [ "$severity" == "error" ]; then
+        printf "critical"
+    elif [ "$severity" == "warning" ]; then
+        printf "major"
+    elif [ "$severity" == "info" ]; then
+        printf "minor"
+    else
+        printf "info"
+    fi
+}
+
+function err() {
+    printf "%s\n" "$*" >&2
+}
+
+file=""
+first=1
+path="$(pwd)"
+printf "[\n"
+tail -n +3 $1 | while read p; do
+    if printf "$p" | grep -q "<file name.*" -; then
+        file="$(realpath "$(expr "$p" : '<file name=\"\(.*\)\".*')")"
+        err Processing checkstyle results for "$file"
+    fi
+    if printf "$p" | grep -q "<error.*" -; then
+        line="$(expr "$p" : '.*line=\"\([0-9]*\)\".*')"
+        message="$(expr "$p" : '.*message=\"\(.*\)\" source.*' | sed -e 's/&apos;/`/g' -e 's/&lt;/</g' -e 's/&gt;/>/g' -e 's/&quot;/\\\"/g' -e 's/&amp;/\&/g')"
+        severityCheckstyle="$(expr "$p" : '.*severity=\"\(.*\)\" message.*')"
+        severity=$(mapSeverity "$severityCheckstyle")
+        checksum=$(printf "%s %s %s\n" "$file" "$line" "$message" | shasum | awk '{print $1}')
+        if [ "$first" == 1 ]; then
+            printf '{ "description": "%s", "severity": "%s", "fingerprint": "%s", "location": { "path": "%s", "lines": { "begin": %d } } }\n' "$message" "$severity" "$checksum" "$file" "$line"
+            first=0
+        else
+            printf ',{ "description": "%s", "severity": "%s", "fingerprint": "%s", "location": { "path": "%s", "lines": { "begin": %d } } }\n' "$message" "$severity" "$checksum" "$file" "$line"
+        fi
+    fi
+done
+printf "]\n"
---
+++ scripts/ci/convert-checkstyle.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+./scripts/ci/checkstyle-to-gitlab.sh ./build/reports/checkstyle/main.xml > ./build/reports/checkstyle/main.json
