88fc0d3d547b3ae0e3fe549702a4c6d2f31267c5
Devin Rosenbauer <devin@instrumentalid.com>
2025-09-29 12:04:07 -0400

    New CoreStable annotation, promising that the tagged APIs will remain stable

---
+++ src/com/identityworksllc/iiq/common/annotation/CoreStable.java
@@ -0,0 +1,13 @@
+package com.identityworksllc.iiq.common.annotation;
+
+/**
+ * Indicates that a class or method is considered a core part of the library,
+ * and will remain API-compatible. This is a promise that future versions
+ * of the library will not break compatibility with this class or method.
+ *
+ * If an annotated class or method must be removed or changed in a way that breaks
+ * compatibility, it will be deprecated for at least six months before being removed
+ * or changed.
+ */
+public @interface CoreStable {
+}
